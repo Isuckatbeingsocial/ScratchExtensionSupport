d80034114e42953f370bb9ec3ee1ef4f08464a0f
Isuckatbeingsocial <88560806+Isuckatbeingsocial@users.noreply.github.com>
2024-06-23 12:03:46 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,69 @@
+class extensionapi {
+    constructor() {
+        this.extensions = {
+            register: function(e) {
+                vm.extensionManager._registerInternalExtension(e);
+            },
+            unsandboxed: true
+        }
+
+        this.ArgumentType = {
+            "ANGLE": "angle",
+            "BOOLEAN": "Boolean",
+            "COLOR": "color",
+            "NUMBER": "number",
+            "STRING": "string",
+            "MATRIX": "matrix",
+            "NOTE": "note",
+            "IMAGE": "image",
+            "COSTUME": "costume",
+            "SOUND": "sound"
+        }
+        this.BlockType = {
+            "BOOLEAN": "Boolean",
+            "BUTTON": "button",
+            "LABEL": "label",
+            "COMMAND": "command",
+            "CONDITIONAL": "conditional",
+            "EVENT": "event",
+            "HAT": "hat",
+            "LOOP": "loop",
+            "REPORTER": "reporter",
+            "XML": "xml"
+        }
+    }
+
+    canEmbed() {
+        return true;
+    }
+
+    canFetch() {
+        return true;
+    }
+
+    canGeolocate() {
+        return true;
+    }
+
+    canOpenWindow() {
+        return true;
+    }
+
+    canReadClipboard() {
+        return true;
+    }
+
+    canRecordAudio() {
+        return true;
+    }
+
+    canRecordVideo() {
+        return true;
+    }
+
+    canRedirect() {
+        return true;
+    }
+}
+
+Scratch = new extensionapi();
